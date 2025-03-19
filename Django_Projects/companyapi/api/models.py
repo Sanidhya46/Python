96208349b0af9ace99eb077a4981ebb9ddf1906e
Sandhya Shukla <sanidhyashukla46@gmail.com>
2025-03-19 23:48:06 +0530

    Your commit message

--- Django_Projects/companyapi/api/models.py
+++ Django_Projects/companyapi/api/models.py
@@ -10,9 +10,30 @@ class Company(models.Model):  #models.Model is from object relation mapper which
     about = models.CharField(max_length=50)
     type = models.CharField( max_length=50,choices = (('IT','IT'),
                                                       ('Non IT', 'Non IT'), 
-                                                      ('Mobile Phones','Mobile Phones')
+                                                      ('Mobile Phones','Mobile Phones'),
+                                                      ('Software Engineers','Soft')
                                                       ))
     added_date = models.DateTimeField(auto_now=True)  # It stores the time when your object is modified
-    active = models.CharField(default=True)   #If no values is specified during object creation 
+    active = models.CharField(max_length=50,default=True)   #If no values is specified during object creation 
+
+
+
 
 #Employee model 
+
+class Employee(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    phone = models.CharField(max_length=10)
+    address = models.CharField(max_length=50)
+    about = models.TextField()
+    position = models.CharField(max_length=50, choices=(
+        ('manager', 'manager'),
+        ('software engineer','software engineer'),
+        ('Project manager','Pl')
+    ))
+
+    # In company there can be many employee model so we are going to use primary key one to many 
+    # if you are writing inside other class then each instance of this model is linked with single instance of company
+    # Foreign key comes from the company class
+    company = models.ForeignKey(Company,on_delete=models.CASCADE)
--- Django_Projects/companyapi/api/serializers.py
+++ Django_Projects/companyapi/api/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from api.models import Company   # we are including all models of Company from api.models
 
 class CompanySerializer(serializers.HyperlinkedModelSerializer):
+    company_id = serializers.ReadOnlyField()  # only reads not modify by api 
     class Meta:  # it stores metadata of the serializer, it tells django how the serializers should behave which models it map to 
         model = Company
         fields="__all__"
