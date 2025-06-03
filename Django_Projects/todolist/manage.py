4d52a3d2e623674d4c09f46e00b6a33cd68e0b57
Sanidhya46 <168342496+Sanidhya46@users.noreply.github.com>
2025-06-03 21:57:19 +0530

    Update manage.py

--- Django_Projects/todolist/manage.py
+++ Django_Projects/todolist/manage.py
@@ -7,11 +7,11 @@ import sys
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todolist.settings')
-    try:
+    try:     
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
+            "Couldn't import Django. Are you sure it's installed and "      
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
