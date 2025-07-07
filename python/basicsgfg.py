2affe11697972a5fc3df56cae1be876a6b01b367
Sanidhya Shukla <168342496+Sanidhya46@users.noreply.github.com>
2025-07-07 06:21:10 +0530

    Update basicsgfg.py

--- python/basicsgfg.py
+++ python/basicsgfg.py
@@ -6,7 +6,7 @@ print("hello world")
 if 10>5:
     print("10 is greater than 5")
 else:
-    print("10 is less than 5")    
+    print("10 is less than 5")       
 
 # Variables
 x = 5                 
